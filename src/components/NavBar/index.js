569d3bbaa847aadfea0300c4fd6562de5cfaca0e
augusttom <auguste.tomaseviciute@gmail.com>
2022-04-26 17:11:45 +0100

    add navigation animation

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -8,10 +8,12 @@ export default () => {
 	return (
 		<>
 			<nav className={classNames(styles.navbar, {[styles.open]: open})}>
-				<Anchor anchor="mission">Mission</Anchor>
-				<Anchor anchor="portfolio">Portfolio</Anchor>
-				<Anchor anchor="experience">experience</Anchor>
-				<Anchor anchor="skills">skills</Anchor>
+				<div className={styles.center}>
+					<Anchor anchor="mission">Mission</Anchor>
+					<Anchor anchor="portfolio">Portfolio</Anchor>
+					<Anchor anchor="experience">experience</Anchor>
+					<Anchor anchor="skills">skills</Anchor>
+				</div>
 				<div className={styles.toggle} onClick={() => setOpen(!open)}/>
 			</nav>
 
