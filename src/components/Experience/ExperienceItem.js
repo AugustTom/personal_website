a6289b93b46b91db582075ad3ac7eee0afa524cc
augusttom <auguste.tomaseviciute@gmail.com>
2022-04-27 18:25:37 +0100

    temp remove skills section from experience

--- src/components/Experience/ExperienceItem.js
+++ src/components/Experience/ExperienceItem.js
@@ -6,18 +6,19 @@ export default ({
 	title,
 	company,
 	description = [],
-	skills= [],
+	// skills= [],
 }) => {
 	return (
 		<FullPage>
 			<div className='experienceItem' >
 				<h5 className='date'>{date}</h5>
 				<h3 className='title'>{title}</h3>
-				<h4 className='company'>{company}</h4>
+				<h4 className='company'><span>@</span>{company}</h4>
 				<ul>
 					{description.map((item, key) => <li key={key}>{item}</li>)}
 				</ul>
-				{skills.map((skill, key) => <span key={key} className='skill'>{skill}</span>)}
+				{/*TODO figure out how display skills*/}
+				{/*{skills.map((skill, key) => <span key={key} className='skill'>{skill}</span>)}*/}
 			</div>
 		</FullPage>
 	);
