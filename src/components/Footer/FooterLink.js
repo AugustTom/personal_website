a9b675f496b675b596224dcf92d8812ae442160f
augusttom <auguste.tomaseviciute@gmail.com>
2022-04-27 19:30:50 +0100

    improve footer

--- src/components/Footer/FooterLink.js
+++ src/components/Footer/FooterLink.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import style from './FooterLink.module.css'
+import React from 'react';
+import style from './FooterLink.module.css';
 
-export default ({href,children}) => {
-    return (
-        <h3 className={style.link}>
-            <a href={href}> {children}</a>
-        </h3>
-    )
-}
+export default ({href, children}) => {
+	return (
+		<h5 className={style.link}>
+			<a href={href}> {children}</a>
+		</h5>
+	);
+};
