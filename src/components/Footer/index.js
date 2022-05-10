3e3363f169c27f9428179c7290f3e338f10395ee
augusttom <auguste.tomaseviciute@gmail.com>
2022-05-10 18:56:51 +0100

    add background

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,10 +4,12 @@ import styles from './Footer.module.css';
 
 export default () => {
 	return (
-		<div className={styles.footer} data-testid={'footer'}>
-			<FooterLink href={'https://www.linkedin.com/in/augusttom/'}>LinkedIn</FooterLink>
-			<FooterLink href={'https://github.com/AugustTom'}>Github</FooterLink>
-			<FooterLink href={'https://medium.com/@augusttomaeviit'}>Medium</FooterLink>
+		<div className={styles.background}>
+			<div className={styles.footer} data-testid={'footer'}>
+				<FooterLink href={'https://www.linkedin.com/in/augusttom/'}>LinkedIn</FooterLink>
+				<FooterLink href={'https://github.com/AugustTom'}>Github</FooterLink>
+				<FooterLink href={'https://medium.com/@augusttomaeviit'}>Medium</FooterLink>
+			</div>
 		</div>
 	);
 };
--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import Typewriter from 'typewriter-effect';
 import FullPage from '../FullPage';
-// import style from './Intro.module.css';
+import style from './Intro.module.css';
 
 export default () => {
 	return (
-		<FullPage label='intro'>
-			<h1> Hi I am </h1>
-			<h1>
-				<Typewriter
-					options={{
-						strings: ['Auguste', 'a developer', 'a learner', 'a maker'],
-						autoStart: true,
-						loop: true,
-					}}
-				/>
-			</h1>
-		</FullPage>
+		<div className={style.background}>
+			<FullPage label='intro'>
+
+				<h1> Hi I am </h1>
+				<h1>
+					<Typewriter
+						options={{
+							strings: ['Auguste', 'a developer', 'a learner', 'a maker'],
+							autoStart: true,
+							loop: true,
+						}}
+					/>
+				</h1>
+
+			</FullPage>
+		</div>
 	);
 };
 
